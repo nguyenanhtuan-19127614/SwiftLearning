468f6b70288f858f96e982e002e947f7b3276563
Nguyễn Anh Tuấn <nguyenanhtuan25122001@gmail.com>
2022-08-26 10:54:51 +0700

    xib test

---
+++ RxSwift/LoanSummaryWithXIB/LoanSummaryWithXIB/View/ConsumerDurable/LoanTopView/LoanTopView.swift
@@ -0,0 +1,34 @@
+//
+//  LoanTopView.swift
+//  LoanSummaryWithXIB
+//
+//  Created by Wee on 26/08/2022.
+//
+
+import UIKit
+
+class LoanTopView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    let XIB_NAME = "LoanTopView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+            
+    }
+    
+    func commonInit() {
+        
+        Bundle.main.loadNibNamed(XIB_NAME, owner: self)
+        contentView.fixInView(self)
+        backgroundColor = .lightGray
+    }
+}
---
+++ RxSwift/LoanSummaryWithXIB/LoanSummaryWithXIB/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoanSummaryWithXIB
+//
+//  Created by Wee on 26/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var topView: LoanTopView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+
+}
+
